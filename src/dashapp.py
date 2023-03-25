df7b3e5443698404007ee92aaff8f730902094c7
alejandro.escobar1 <alejandro.escobar1@ucalgary.ca>
2023-03-25 13:56:03 -0600

    added server var

--- src/dashapp.py
+++ src/dashapp.py
@@ -6,11 +6,11 @@ from numpy import polyfit,linspace,poly1d
 from pandas import read_csv
 import dash_bootstrap_components as dbc
 from os import path,environ
-from flask import Flask
 from pymongo import MongoClient
 
 #flask_server = Flask(__name__)
 app = Dash(__name__,suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 app.title = "NFL Defensive Trajectory Prediction"
 
 my_dir = path.dirname(__file__)
